39adec351e1b467ee048764f9f89222eaca7291b
VasantiGaikwad <vasantisankpal95@gmail.com>
2023-10-25 11:59:26 +0530

    new

---
+++ SecProject-2/src/main/java/com/example/demo/SecProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecProject2Application.class, args);
+	}
+
+}
